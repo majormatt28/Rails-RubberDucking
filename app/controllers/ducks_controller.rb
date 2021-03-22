c90d1893fa6316dfa6c84c64527d3634e73e16f3
Matthew <elijah2895@hotmail.com>
2021-03-22 09:21:23 -0400

    done

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,37 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def new
+    @duck - Duck.new
+  end
+
+  def edit
+    @duck = Duck.find(params[:id])
+
+    render :edit
+  end
+
+  def create
+    @duck = Duck.create(duck_params)
+
+    redirect_to duck_path(@duck.id)
+  end
+
+  def update    
+    @duck = Duck.find(params[:id])
+    @duck.update(duck_params)
+
+    redirect_to duck_path(@duck)
+  end
+
+  private
+
+  def duck_params
+    params.require(:duck).permit(:name, :description)
+  end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,23 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+  
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+
+    redirect_to student_path(@student.id)
+  end
+
+  private
+
+  def student_params
+    params.require(:student).(:name, :mod)
+  end
 end
